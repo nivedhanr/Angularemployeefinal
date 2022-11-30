d91e2c53dbe168ed1bd399cce5d0c62d85e4e5f6
nivedhanr <nivedharenahan@gmail.com>
2022-12-01 00:25:39 +0530

    authentication

--- src/app/empregister/empregister.component.ts
+++ src/app/empregister/empregister.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empregister',
@@ -6,5 +7,100 @@ import { Component } from '@angular/core';
   styleUrls: ['./empregister.component.css']
 })
 export class EmpregisterComponent {
+  empid=""
+  firstname=""
+  lastname=""
+  houseno=""
+  housename=""
+  streetname=""
+  pincode=""
+  district=""
+  state=""
+  country=""
+  mobileno=""
+  emailid=""
+  parentname=""
+  gender=""
+  highestdegree=""
+  yearofexperience=""
+  dateofjoining=""
+  username=""
+  password=""
+  confirmpass=""
+  constructor(private route:Router){}
+  readValues=()=>
+  {
+    let data:any={
+
+      "employeeid":this.empid,
+
+      "firstname":this.firstname,
+
+      "lastname":this.lastname,
+
+      "houseno":this.houseno,
+
+      "housename":this.housename,
+
+      "streetname":this.streetname,
+
+      "pincode":this.pincode,
+
+      "district":this.district,
+
+      "state":this.state,
+
+      "country":this.country,
+
+      "mobile":this.mobileno,
+
+      "email":this.emailid,
+
+      "parentname":this.parentname,
+
+      "gender":this.gender,
+
+      "highestdegree":this.highestdegree,
+
+      "yearofexperience":this.yearofexperience,
+
+      "dateofjoining":this.dateofjoining,
+
+      "username":this.username,
+
+      "password":this.password,
+
+      "confirmpassword":this.confirmpass
+
+    }
+    console.log(data)
+    if(this.confirmpass==this.password) {
+      alert("Successfully registered")
+      this.confirmpass=""
+      this.country=""
+      this.dateofjoining=""
+      this.district=""
+      this.emailid=""
+      this.empid=""
+      this.firstname=""
+      this.gender=""
+      this.highestdegree=""
+      this.housename=""
+      this.houseno=""
+      this.lastname=""
+      this.mobileno=""
+      this.parentname=""
+      this.password=""
+      this.pincode=""
+      this.state=""
+      this.streetname=""
+      this.username=""
+      this.yearofexperience=""
+      
+    } else {
+      alert("Password and confirmpassword doesnot match")
+    }
+  }
+
 
 }
