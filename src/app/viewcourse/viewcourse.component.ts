67555f1be1ecc0194a53fc4dfbb1bea5b7d56d6e
nivedhanr <nivedharenahan@gmail.com>
2022-11-30 17:19:21 +0530

    viewcourse

---
+++ src/app/viewcourse/viewcourse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcourse',
+  templateUrl: './viewcourse.component.html',
+  styleUrls: ['./viewcourse.component.css']
+})
+export class ViewcourseComponent {
+  data:any=[]
+
+}
