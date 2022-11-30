630a05c4eebc0a056c6e130cc9ab41ed23cea18b
nivedhanr <nivedharenahan@gmail.com>
2022-11-30 16:30:40 +0530

    employee registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { EmpregisterComponent } from './empregister/empregister.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { EmployeeComponent } from './employee/employee.component';
     GalleryComponent,
     ContactComponent,
     AboutComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    EmpregisterComponent
   ],
   imports: [
     BrowserModule,
