c61acd5e0d33e852e5c0ebb62913d6d765ee428d
nivedhanr <nivedharenahan@gmail.com>
2022-11-30 17:38:44 +0530

    viewfriend

---
+++ src/app/viewfriend/viewfriend.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewfriend',
+  templateUrl: './viewfriend.component.html',
+  styleUrls: ['./viewfriend.component.css']
+})
+export class ViewfriendComponent {
+  data:any=[]
+
+}
