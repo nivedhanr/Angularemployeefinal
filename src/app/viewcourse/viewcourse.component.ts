033b181f22b6711771d6536625f619abc121ff0b
nivedhanr <nivedharenahan@gmail.com>
2022-12-01 10:54:11 +0530

    loadingscreen

--- src/app/viewcourse/viewcourse.component.ts
+++ src/app/viewcourse/viewcourse.component.ts
@@ -11,9 +11,11 @@ export class ViewcourseComponent {
   
  constructor(private api:ApiService){
   api.fetchcourse().subscribe((response)=>{
+    this.loading=false
     this.data=response;
   })
  }
   data:any=[]
+  loading:boolean=true
 }
 
